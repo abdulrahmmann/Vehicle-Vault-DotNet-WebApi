07ae43082c54aea002b912ae49e5a08cb30ae37d
Abdulrahman <abd3lra7man.078@gmail.com>
2025-08-03 23:43:36 +0300

    Create RegisterUser Request.

---
+++ VehicleVault.Application/Features/UserFeature/Commands/Requests/RegisterUserRequest.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using VehicleVault.Application.Common;
+using VehicleVault.Application.Features.UserFeature.DTOs;
+
+namespace VehicleVault.Application.Features.UserFeature.Commands.Requests;
+
+public record RegisterUserRequest(RegisterUserDto UserDto): IRequest<AuthenticationResponse>;
