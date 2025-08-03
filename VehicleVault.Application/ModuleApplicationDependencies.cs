2defda8efda801cc38296f66a0cce12cf097ff54
Abdulrahman <abd3lra7man.078@gmail.com>
2025-08-03 18:18:18 +0300

    Setup Application Layer, Adding IServiceCollection Class.

---
+++ VehicleVault.Application/ModuleApplicationDependencies.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace VehicleVault.Application;
+
+public static class ModuleApplicationDependencies
+{
+    public static IServiceCollection AddApplicationDependencies(this IServiceCollection services)
+    {
+        // Register Mediator
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        
+        // Register JWT Services
+        
+        // Register FLUENT VALIDATION
+        
+        return services;
+    }
+}
