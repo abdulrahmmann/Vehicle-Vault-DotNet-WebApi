f81f5d19a60470893bb65a27e6356fe3e7166cb2
Abdulrahman <abd3lra7man.078@gmail.com>
2025-08-08 02:03:14 +0300

    Create Migration "AddIsDeleteFieldSoftDeletion".

---
+++ VehicleVault.Infrastructure/Migrations/20250807230138_Add-IsDelete-Field-SoftDeletion.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VehicleVault.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsDeleteFieldSoftDeletion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Vehicles",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "VehicleImages",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "VehicleFeatures",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "TransmissionTypes",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "SubModels",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Models",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Makes",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "FuelTypes",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Features",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "DriveTypes",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Colors",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Categories",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Bodies",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Vehicles");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "VehicleImages");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "VehicleFeatures");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "TransmissionTypes");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "SubModels");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Models");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Makes");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "FuelTypes");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Features");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "DriveTypes");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Colors");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Bodies");
+        }
+    }
+}
