9bf9e29514203f659f2270b18e0144626945fcae
Abdulrahman <abd3lra7man.078@gmail.com>
2025-08-08 03:33:43 +0300

    Create CreateCategoriesRange Endpoint.

--- VehicleVault.Api/Controllers/CategoryController.cs
+++ VehicleVault.Api/Controllers/CategoryController.cs
@@ -1,3 +1,4 @@
+using Asp.Versioning;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using VehicleVault.Application.Features.CategoryFeature.Commands.Requests;
@@ -8,6 +9,7 @@ namespace VehicleVault.Controllers
     [AllowAnonymous]
     [Route("api/[controller]")]
     [ApiController]
+    [ApiVersion("1.0")]
     public class CategoryController : AppControllerBase
     {
         [HttpPost]
@@ -21,6 +23,20 @@ namespace VehicleVault.Controllers
 
             var result = await Mediator.Send(new CreateCategoryRequest(categoryDto));
 
+            return NewResult(result);
+        }
+
+        [HttpPost]
+        [Route("create-category-list")]
+        public async Task<ActionResult> CreateCategoriesRange([FromBody] IEnumerable<CategoryDto> categoriesDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            
+            var result = await Mediator.Send(new CreateCategoriesRangeRequest(categoriesDto));
+            
             return NewResult(result);
         }
     }
---
+++ VehicleVault.Application/Features/CategoryFeature/Commands/Handler/CreateCategoriesRangeHandler.cs
@@ -0,0 +1,95 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using VehicleVault.Application.Common;
+using VehicleVault.Application.Features.CategoryFeature.Commands.Requests;
+using VehicleVault.Application.Features.CategoryFeature.DTOs;
+using VehicleVault.Domain.Entities;
+using VehicleVault.Infrastructure.UOF;
+
+namespace VehicleVault.Application.Features.CategoryFeature.Commands.Handler;
+
+public class CreateCategoriesRangeHandler: IRequestHandler<CreateCategoriesRangeRequest, BaseResponse<Unit>>
+{
+    #region Instance Fields
+    private readonly IUnitOfWork  _unitOfWork;
+    private readonly IValidator<CategoryDto> _validator;
+    private readonly ILogger<CreateCategoriesRangeHandler> _logger;
+    #endregion
+
+    #region Constructor
+    public CreateCategoriesRangeHandler(IUnitOfWork unitOfWork, IValidator<CategoryDto> validator, 
+        ILogger<CreateCategoriesRangeHandler> logger) 
+    {
+        _unitOfWork = unitOfWork;
+        _logger = logger;
+        _validator = validator;
+    }
+    #endregion
+
+    public async Task<BaseResponse<Unit>> Handle(CreateCategoriesRangeRequest request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var validCategoriesToAdd = new List<CategoryDto>();
+            var invalidCategoriesToRemove = new List<string>();
+            
+            foreach (var category in request.CategoriesDto)
+            {
+                // check validation input
+                var validationResult = await _validator.ValidateAsync(category, cancellationToken);
+                if (!validationResult.IsValid)
+                {
+                    var validationErrors = string.Join(",", validationResult.Errors.Select(x => x.ErrorMessage));
+                    _logger.LogWarning("validation error {error}", validationErrors);
+                    invalidCategoriesToRemove.Add(category.Name);
+                    continue;
+                }
+
+                // check existing category
+                var existingCategory = await _unitOfWork.GetCategoryRepository.ExistsByNameAsync(category.Name);
+                if (existingCategory)
+                {
+                    _logger.LogWarning("Category with name: {name} already exists", category.Name);
+                    invalidCategoriesToRemove.Add(category.Name);
+                    continue;
+                }
+                
+                // add valid categories
+                validCategoriesToAdd.Add(category);
+            }
+            
+            var enumerableCategories = validCategoriesToAdd.AsEnumerable();
+            
+            var categoriesMapped = enumerableCategories
+                .Select(c => new Category(c.Name, c.Description));
+            
+            if (validCategoriesToAdd.Count == 0)
+            {
+                _logger.LogWarning("No valid categories to add");
+                return BaseResponse<Unit>.BadRequest("No valid categories to add");
+            }
+
+            if (invalidCategoriesToRemove.Count != 0)
+            {
+                _logger.LogWarning("some invalid categories not added: {invalidCategories}", invalidCategoriesToRemove.ToList());
+            }
+            
+            await _unitOfWork.GetRepository<Category>().AddRangeAsync(categoriesMapped);
+            await _unitOfWork.SaveChangesAsync();
+
+            if (validCategoriesToAdd.Count != 0 && invalidCategoriesToRemove.Count != 0)
+            {
+                return BaseResponse<Unit>
+                    .Success($"{validCategoriesToAdd.Count} valid categories added and {invalidCategoriesToRemove.Count} invalid categories");
+            }
+            
+            return BaseResponse<Unit>.Success($"All Categories were created: {validCategoriesToAdd.Count} / {request.CategoriesDto.Count()}");
+        }
+        catch (Exception e)
+        {
+            _logger.LogError("An error occured while creating category: {error}", e.Message);
+            return BaseResponse<Unit>.InternalError($"An error occured while creating category: {e.Message}");
+        }
+    }
+}
